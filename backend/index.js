45ac0fac7cfe06ba99f87929134e79b794f46cb2
Venkat Rohith Saripalli <venkatrohithcse63@gmail.com>
2022-12-10 22:42:34 +0530

    initial express & mongoose set up heartbeat api extract env variables into a file for sharing

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,37 @@
-console.log("Initial Setup");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const {
+  client_instance_url,
+  port,
+  db_conn_string,
+  db_name,
+} = require("../env-variables");
+
+const app = express();
+
+//middleware
+app.use(
+  cors({
+    origin: client_instance_url,
+    methods: "GET,PATCH,POST,DELETE",
+  })
+);
+app.use(express.json());
+
+// Hearbeat api
+app.get("/api/heartbeat", (_, res) => {
+  res.status(200).json("Yaah🤩, Server is up🌟⬆️");
+});
+
+//connect to DB
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(`${db_conn_string}/${db_name}`)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Connected to DB & listening on port ${port}`);
+    });
+  })
+  .catch((error) => console.log(error));
