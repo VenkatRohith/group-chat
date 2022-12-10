6fdbed80ed45fee2d8d3cbcab1ea32ff7c05a977
Venkat Rohith Saripalli <venkatrohithcse63@gmail.com>
2022-12-10 22:42:34 +0530

    initial express & mongoose set up heartbeat api extract env variables into a file for sharing

---
+++ env-variables.js
@@ -0,0 +1,7 @@
+module.exports = {
+  port: process.env.PORT,
+  db_conn_string: process.env.DB_CONN_STRING,
+  db_name: process.env.DB_NAME,
+  secret: process.env.SECRET,
+  client_instance_url: process.env.CLIENT_INSTANCE_URL,
+};
