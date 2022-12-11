74462367f65d2e72dfb6f44136fffdc82368a315
Venkat Rohith Saripalli <venkatrohithcse63@gmail.com>
2022-12-11 15:58:41 +0530

    initial controller declarations setup

---
+++ backend/controllers/groupController.js
@@ -0,0 +1,14 @@
+const createGroup = () => {};
+
+const deleteGroup = () => {};
+
+const searchUserinGroup = () => {};
+
+const addUserToGroup = () => {};
+
+module.exports = {
+  createGroup,
+  deleteGroup,
+  searchUserinGroup,
+  addUserToGroup,
+};
---
+++ backend/controllers/userController.js
@@ -0,0 +1,7 @@
+const createUser = () => {};
+
+const editUser = () => {};
+
+const getUsers = () => {};
+
+module.exports = { createUser, editUser, getUsers };
