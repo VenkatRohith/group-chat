bdce912c1e776305e1a7be4dd8dc7cc2fc9cb1d8
Venkat Rohith Saripalli <venkatrohithcse63@gmail.com>
2022-12-11 15:58:41 +0530

    initial controller declarations setup

---
+++ backend/controllers/messageController.js
@@ -0,0 +1,5 @@
+const sendMessage = () => {};
+
+const likeMessage = () => {};
+
+module.exports = { sendMessage, likeMessage };
---
+++ backend/controllers/userLoginController.js
@@ -0,0 +1,5 @@
+const login = () => {};
+
+const logout = () => {};
+
+module.exports = { login, logout };
